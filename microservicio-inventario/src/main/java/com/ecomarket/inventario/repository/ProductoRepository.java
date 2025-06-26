2cc1954838cb8e33e7eba67538f3861529a587eb
fabian <101838320+fabianquinones@users.noreply.github.com>
2025-06-25 20:52:36 -0400

    Delete microservicio-inventario directory

--- microservicio-inventario/src/main/java/com/ecomarket/inventario/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecomarket.inventario.repository;
-
-import com.ecomarket.inventario.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
