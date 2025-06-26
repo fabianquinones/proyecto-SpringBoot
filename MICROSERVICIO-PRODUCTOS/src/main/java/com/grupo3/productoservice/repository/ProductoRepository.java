2f82f88c7b5a08830ab7d5b5e80f18ea4914b402
fabian <101838320+fabianquinones@users.noreply.github.com>
2025-06-25 20:37:36 -0400

    Delete MICROSERVICIO-PRODUCTOS directory

--- MICROSERVICIO-PRODUCTOS/src/main/java/com/grupo3/productoservice/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.grupo3.productoservice.repository;
-
-import com.grupo3.productoservice.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
