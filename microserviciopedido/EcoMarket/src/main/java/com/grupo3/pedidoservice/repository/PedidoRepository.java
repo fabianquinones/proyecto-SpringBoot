3249568234fbb81a9504d5df69cdef42bd40dc2f
fabian <101838320+fabianquinones@users.noreply.github.com>
2025-06-25 20:52:13 -0400

    Delete microserviciopedido/EcoMarket directory

--- microserviciopedido/EcoMarket/src/main/java/com/grupo3/pedidoservice/repository/PedidoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.grupo3.pedidoservice.repository;
-
-import com.grupo3.pedidoservice.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {}
